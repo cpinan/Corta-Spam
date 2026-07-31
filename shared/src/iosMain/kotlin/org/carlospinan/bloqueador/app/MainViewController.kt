6d5ac28dd9096929adc70854e4f01fa8ea146082
Carlos Eduardo Piñán Indacochea <carlos.pinan@gmail.com>
2026-07-30 22:06:08 -0500

    fix: iOS CADisableMinimumFrameDurationOnPhone in Info.plist
    - Compose Multiplatform requires this key set to true on iPhones
      with high refresh rate displays for adequate performance.
    - Without it, the plist sanity check throws IllegalStateException.
    - Fixed in xcodegen project.yml info.plist properties.

--- shared/src/iosMain/kotlin/org/carlospinan/bloqueador/app/MainViewController.kt
+++ shared/src/iosMain/kotlin/org/carlospinan/bloqueador/app/MainViewController.kt
@@ -1,11 +1,16 @@
 package org.carlospinan.bloqueador.app
 
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.PlatformContext
 import androidx.compose.ui.window.ComposeUIViewController
 import org.carlospinan.bloqueador.app.di.initKoin
 import platform.UIKit.UIViewController
 
 @Suppress("FunctionName")
+@OptIn(ExperimentalComposeUiApi::class)
 fun MainViewController(): UIViewController {
     initKoin()
-    return ComposeUIViewController { App() }
+    return ComposeUIViewController {
+        App()
+    }
 }
